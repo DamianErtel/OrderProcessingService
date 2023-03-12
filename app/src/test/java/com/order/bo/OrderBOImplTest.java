1eed352c88257ff0a478de30af5fd1fc37f90da5
Damian Ertel <damianertel@MacBook-Air-Damian.local>
2023-03-12 11:45:29 +0100

    add more unit tests

--- app/src/test/java/com/order/bo/OrderBOImplTest.java
+++ app/src/test/java/com/order/bo/OrderBOImplTest.java
@@ -5,13 +5,17 @@ import static org.mockito.Mockito.anyInt;
 import static org.mockito.Mockito.atLeast;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.never;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.sql.SQLException;
 
+import org.junit.Ignore;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
@@ -34,7 +38,7 @@ class OrderBOImplTest {
     }
 
     @Test
-    void placeOrder_Shoud_Create_An_Order() throws SQLException, BOException {
+    void placeOrder_Should_Create_An_Order() throws SQLException, BOException {
         Order order = new Order();
         Integer integer = Integer.valueOf(1);
         when(dao.create(any(Order.class))).thenReturn(integer);
@@ -44,7 +48,7 @@ class OrderBOImplTest {
     }
 
     @Test
-    void placeOrder_Shoud_not_Create_An_Order() throws SQLException, BOException {
+    void placeOrder_Should_not_Create_An_Order() throws SQLException, BOException {
         Order order = new Order();
         Integer integer = Integer.valueOf(0);
         when(dao.create(any(Order.class))).thenReturn(integer);
@@ -74,7 +78,7 @@ class OrderBOImplTest {
     }
 
     @Test
-    void cancelOrder_Shoud_NOT_Cancel_An_Order() throws SQLException, BOException {
+    void cancelOrder_Should_NOT_Cancel_An_Order() throws SQLException, BOException {
         Order order = new Order();
         when(dao.read(ORDER_ID)).thenReturn(order);
         when(dao.update(order)).thenReturn(0);
@@ -103,7 +107,7 @@ class OrderBOImplTest {
     }
 
     @Test
-    void deleteOrder_Shoud_Delete_An_Order() throws SQLException, BOException {
+    void deleteOrder_Should_Delete_An_Order() throws SQLException, BOException {
         when(dao.delete(ORDER_ID)).thenReturn(1);
         boolean result = bo.deleteOrder(ORDER_ID);
         assertTrue(result);
@@ -125,4 +129,133 @@ class OrderBOImplTest {
         boolean result = bo.modifyOrder(ORDER_ID, order);
         assertFalse(result);
     }
+
+
+    // Zadanie 16
+    @Test
+    void modifyOrder_Should_Throw_BOException() throws SQLException, BOException {
+        Order order = new Order();
+        when(dao.update(order)).thenThrow(SQLException.class);
+        Assertions.assertThrows(BOException.class, () -> {
+            bo.modifyOrder(ORDER_ID, order);
+        });
+    }
+
+    @Test
+    void modifyOrder_Should_Not_Throw_Exception() throws SQLException, BOException {
+        Order order = new Order();
+        when(dao.update(order)).thenReturn(1);
+        Assertions.assertDoesNotThrow(() -> {
+            bo.modifyOrder(ORDER_ID, order);
+        });
+    }
+
+    @Test
+    void createOrder_Should_Throw_BOException() throws SQLException, BOException {
+        Order order = new Order();
+        when(dao.create(order)).thenThrow(SQLException.class);
+        Assertions.assertThrows(BOException.class, () -> {
+            bo.placeOrder(order);
+        });
+    }
+
+    @Test
+    void createOrder_Should_Not_Throw_Exception() throws SQLException, BOException {
+        Order order = new Order();
+        when(dao.create(order)).thenReturn(1);
+        Assertions.assertDoesNotThrow(() -> {
+            bo.placeOrder(order);
+        });
+    }
+
+    // Zadanie 15
+
+    @Test
+    void modifyOrder_Should_Call_Read() throws SQLException, BOException {
+        when(dao.update(any(Order.class))).thenReturn(1);
+        Order order = new Order();
+        boolean result = bo.modifyOrder(ORDER_ID, order);
+        assertTrue(result);
+        verify(dao, atLeast(1)).read(ORDER_ID);
+    }
+
+    @Test
+    void modifyOrder_Should_Call_Update() throws SQLException, BOException {
+        when(dao.update(any(Order.class))).thenReturn(1);
+        Order order = new Order();
+        boolean result = bo.modifyOrder(ORDER_ID, order);
+        assertTrue(result);
+        verify(dao, atLeast(1)).update(order);
+    }
+
+    @Test
+    void modifyOrder_Should_Not_Call_Create() throws SQLException, BOException {
+        when(dao.update(any(Order.class))).thenReturn(1);
+        Order order = new Order();
+        boolean result = bo.modifyOrder(ORDER_ID, order);
+        assertTrue(result);
+        verify(dao, never()).create(order);
+    }
+
+    @Test
+    void modifyOrder_Should_Not_Call_Delete() throws SQLException, BOException {
+        when(dao.update(any(Order.class))).thenReturn(1);
+        Order order = new Order();
+        boolean result = bo.modifyOrder(ORDER_ID, order);
+        assertTrue(result);
+        verify(dao, never()).delete(anyInt());
+    }
+
+    @Test
+    void placeOrder_Should_Call_Create() throws SQLException, BOException {
+        when(dao.create(any(Order.class))).thenReturn(1);
+        Order order = new Order();
+        boolean result = bo.placeOrder(order);
+        assertTrue(result);
+        verify(dao, atLeast(1)).create(order);
+    }
+
+    @Test
+    void placeOrder_Should_Not_Call_Read() throws SQLException, BOException {
+        when(dao.create(any(Order.class))).thenReturn(1);
+        Order order = new Order();
+        boolean result = bo.placeOrder(order);
+        assertTrue(result);
+        verify(dao, never()).read(anyInt());
+    }
+
+    @Test
+    void placeOrder_Should_Not_Call_Delete() throws  SQLException, BOException {
+        when(dao.create(any(Order.class))).thenReturn(1);
+        Order order = new Order();
+        boolean result = bo.placeOrder(order);
+        assertTrue(result);
+        verify(dao, never()).delete(anyInt());
+    }
+
+    @Test
+    void placeOrder_Should_Not_Call_Update() throws  SQLException, BOException {
+        when(dao.create(any(Order.class))).thenReturn(1);
+        Order order = new Order();
+        boolean result = bo.placeOrder(order);
+        assertTrue(result);
+        verify(dao, never()).update(any(Order.class));
+    }
+
+    @Test
+    void deleteOrder_delete_Should_Accept_Any_Int() throws SQLException, BOException {
+        when(dao.delete(anyInt())).thenReturn(1);
+        boolean result = bo.deleteOrder(2);
+        assertTrue(result);
+    }
+
+    @Test
+    void deleteOrder_Should_Call_Delete() throws SQLException, BOException {
+        when(dao.delete(anyInt())).thenReturn(1);
+        boolean result = bo.deleteOrder(2);
+        assertTrue(result);
+        verify(dao, atLeast(1)).delete(anyInt());
+    }
+
+
 }
